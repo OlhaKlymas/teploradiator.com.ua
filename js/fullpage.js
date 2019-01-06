b94ccfca86e63306e76028449f806addda219a08
OlhaKlymas <olhaklymas@gmail.com>
2019-01-06 10:59:38 +0200

    fullpage

--- js/fullpage.js
+++ js/fullpage.js
@@ -45,7 +45,7 @@
     var COMPLETELY_SEL =        '.' + COMPLETELY;
 
     // section
-    var SECTION_DEFAULT_SEL =   '.section';
+    var SECTION_DEFAULT_SEL =   'section';
     var SECTION =               'fp-section';
     var SECTION_SEL =           '.' + SECTION;
     var SECTION_ACTIVE_SEL =    SECTION_SEL + ACTIVE_SEL;
