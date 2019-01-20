11aa363422352dd6f3837fdb1c80d3aa3950924b
OlhaKlymas <olhaklymas@gmail.com>
2019-01-20 10:40:18 +0200

    add popup

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let myFullpage = new fullpage('#fullpage', {
+      anchors: ['section0', 'section1', 'section2', 'section3', 'section4', 'section5', 'section6'],
+      navigation: true
+    });
+
+$(document).ready(function(){
+    PopUpHide();
+});
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
